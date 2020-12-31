0b531f935a9ae3837fb0bb49aabbc7c61cd0c282
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2020-12-31 21:59:51 +0900

    configure_line_to_credential

--- app/controllers/line_events_controller.rb
+++ app/controllers/line_events_controller.rb
@@ -2,8 +2,8 @@ class LineEventsController < ApplicationController
     require 'line/bot'
     def client
         @client ||= Line::Bot::Client.new { |config|
-          config.channel_secret = "d6d08cedfee7051debd7f09f323ddc91"
-          config.channel_token = "jcUJUDGWVYnU9A3TkQpAjI2G0TCKK7KmMrNr8892E2rROrInXE8SjdpqgG2s0qtwuiYIy7OAjMpG/TSrsxSIj9KjCm0tN8dBN4malFiwllttN6k3ZTqQ3nJLkTtKHbo0wr4o8xgMEQJrgy6lPQ0EkQdB04t89/1O/w1cDnyilFU="
+          config.channel_secret = Rails.application.credentials.line[:channel_secret]
+          config.channel_token = Rails.application.credentials.line[:channel_token]
         }
     end
     def receive
