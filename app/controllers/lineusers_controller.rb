7b26483911115f596a30f5f4581adab29ed52a78
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-02-03 08:17:51 +0900

    link_lineuser

--- app/controllers/lineusers_controller.rb
+++ app/controllers/lineusers_controller.rb
@@ -2,4 +2,13 @@ class LineusersController < ApplicationController
   def new
     @lineuser = Lineuser.new()
   end
+  def create
+    @lineuser = Lineuser.create(lineuser_params)
+    redirect_to messages_path
+  end
+
+  private
+  def lineuser_params
+    params.require(:lineuser).permit(:line_uid, :line_name, :student_id)
+  end
 end
