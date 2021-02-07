d21d0247c8b28b09d237964ca86d1a0663509b88
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-02-07 10:56:35 +0900

    send_line_test

--- app/assets/javascripts/ios.js
+++ app/assets/javascripts/ios.js
@@ -1,13 +1,21 @@
 $(function(){
   $('#ios_enter').on('click',function(e){
-    e.preventDefault();
-    const sendData = {
-      student_id:'',
-      in_time:'',
-      out_time:'',
+    // e.preventDefault();
+    const url = 'https://api.line.me/v2/bot/message/push';
+    const sendBody = {
+      to: gon.line_token,
+      messages:'入室しました'
+      // messages:`${student_name}さんが${stamp_time}に入室されました。`,
     }
-    sessionStorage.setItem('LINEID',line_uid);
-    window.location.href = '/lineusers/new';
+    fetch(url,{
+      method: 'POST',
+      mode: 'no-cors',
+      headers:{
+        'Content-Type' : 'application/json',
+        'Authorization' : 'Bearer {d1c4Y9CggTzklxQ+s44F2osUrTX/1KHvr5W9NwDyJuy4wmbYJ5Da6OsMxZA4PMhCuiYIy7OAjMpG/TSrsxSIj9KjCm0tN8dBN4malFiwlls4RwfLz41BbfHRTn9OwoKefzxwuHwvHUeEKxaqfWQMXgdB04t89/1O/w1cDnyilFU=}',        
+      },
+      body: JSON.stringify(sendBody),
+    });
   });
   $('#ios_exit').on('click',function(){
     const line_uid = $(this).parent().find('span').html();
--- app/controllers/ios_controller.rb
+++ app/controllers/ios_controller.rb
@@ -1,17 +1,19 @@
 class IosController < ApplicationController
   def new
     @io=Io.new
+    gon.line_token = Rails.application.credentials.line[:channel_token]
   end
   def create
-    @io = Io.new
-    if @io.save
-      respond_to do |format|
-        format.html
-        format.json
-      end
+    # @io = Io.new
+    # if @io.save
+    #   respond_to do |format|
+    #     format.html
+    #     format.json
+    #   end
     
-    end
+    # end
   end
   private
   def ios_params
+  end
 end
