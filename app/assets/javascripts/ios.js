1bafe14b347f01658f0a4239eeb953a9847a4b85
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-02-10 23:56:02 +0900

    set_clock

--- app/assets/javascripts/ios.js
+++ app/assets/javascripts/ios.js
@@ -1,21 +1,48 @@
 $(function(){
+
+
+  function showClock() {
+    const week_chars=['日','月','火','水','木','金','土'];
+    let now_time=new Date();
+    let now_month=now_time.getMonth() + 1;
+    let now_date=now_time.getDate();
+    let now_day_of_the_week=now_time.getDay();
+    let now_hour=('0' + now_time.getHours()).slice(-2);
+    let now_minute=('0'+ now_time.getMinutes()).slice(-2);
+    let now_second=('0' + now_time.getSeconds()).slice(-2);
+
+    document.querySelector('#ios_time').textContent=`${now_month}/${now_date}(${week_chars[now_day_of_the_week]}) ${now_hour}:${now_minute}:${now_second}`;
+  }
+
+  setInterval(() => {
+    showClock();
+  }, 1000);
+
+
+  
+  
+  
+
+
+
+
   $('#ios_enter').on('click',function(e){
-    // e.preventDefault();
-    const url = 'https://api.line.me/v2/bot/message/push';
-    const sendBody = {
-      to: gon.line_token,
-      messages:'入室しました'
-      // messages:`${student_name}さんが${stamp_time}に入室されました。`,
-    }
-    fetch(url,{
-      method: 'POST',
-      mode: 'no-cors',
-      headers:{
-        'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer {d1c4Y9CggTzklxQ+s44F2osUrTX/1KHvr5W9NwDyJuy4wmbYJ5Da6OsMxZA4PMhCuiYIy7OAjMpG/TSrsxSIj9KjCm0tN8dBN4malFiwlls4RwfLz41BbfHRTn9OwoKefzxwuHwvHUeEKxaqfWQMXgdB04t89/1O/w1cDnyilFU=}',        
-      },
-      body: JSON.stringify(sendBody),
-    });
+    // // e.preventDefault();
+    // const url = 'https://api.line.me/v2/bot/message/push';
+    // const sendBody = {
+    //   to: 'U0d1607fd7ec247b84f52da09b3ab30e1',
+    //   messages:'入室しました'
+    //   // messages:`${student_name}さんが${stamp_time}に入室されました。`,
+    // }
+    // fetch(url,{
+    //   method: 'POST',
+    //   mode: 'no-cors',
+    //   headers:{
+    //     'Content-Type' : 'application/json',
+    //     'Authorization' : 'Bearer d1c4Y9CggTzklxQ+s44F2osUrTX/1KHvr5W9NwDyJuy4wmbYJ5Da6OsMxZA4PMhCuiYIy7OAjMpG/TSrsxSIj9KjCm0tN8dBN4malFiwlls4RwfLz41BbfHRTn9OwoKefzxwuHwvHUeEKxaqfWQMXgdB04t89/1O/w1cDnyilFU=;wq',        
+    //   },
+    //   body: JSON.stringify(sendBody),
+    // });
   });
   $('#ios_exit').on('click',function(){
     const line_uid = $(this).parent().find('span').html();
--- app/controllers/ios_controller.rb
+++ app/controllers/ios_controller.rb
@@ -12,7 +12,18 @@ class IosController < ApplicationController
     #   end
     
     # end
+    message = {
+      type: 'text',
+      text: '入室しました'
+    }
+    client = Line::Bot::Client.new { |config|
+    config.channel_secret = Rails.application.credentials.line[:channel_secret]
+    config.channel_token = Rails.application.credentials.line[:channel_token]
+    }
+    response = client.push_message('U0d1607fd7ec247b84f52da09b3ab30e1',message)
+    puts response
   end
+  
   private
   def ios_params
   end
