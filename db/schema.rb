b19f46925c67fe740021e5f73d3d1d694d1b60f6
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-04-06 06:46:57 +0900

    logical deletion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_212012) do
+ActiveRecord::Schema.define(version: 2021_04_05_214128) do
 
   create_table "ios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "student_id"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 2021_02_10_212012) do
     t.boolean "student_enabled"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_students_on_deleted_at"
   end
 
   add_foreign_key "ios", "students"
