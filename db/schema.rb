476d27be23d85d92a63a2aa6e58553676f04aff8
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-04-08 21:39:29 +0900

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_214128) do
+ActiveRecord::Schema.define(version: 2021_04_08_123424) do
 
   create_table "ios", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "student_id"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2021_04_05_214128) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "deleted_at"
+    t.string "email"
     t.index ["deleted_at"], name: "index_students_on_deleted_at"
   end
 
